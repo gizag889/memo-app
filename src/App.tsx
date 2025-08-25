ee091cb10b0ac87a346d482e23113d3577f2f13d
gizag889 <gizaguri0426@outlook.jp>
2025-08-25 21:45:43 +0900

    メモ作成フォームの実装- 共通の型を定義- メモ作成ロジックの追加- Editorコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import Layout from "./components/Layout";
-
+import Editor from "./components/Editor";
+import { useState } from "react";
+import type { Memo } from "./types/index";
+ 
+const initialMemo = {
+  id: Date.now(),
+  text: "思いついたことを、サッとメモ。\n作業に集中するための、シンプルなメモアプリです！",
+  timestamp: new Date().toLocaleString("ja-JP"),
+};
+ 
 function App() {
-  return <Layout>hello</Layout>;
+  const [memos, setMemos] = useState<Memo[]>([initialMemo]);
+ 
+  const addMemo = (inputText: string) => {
+    if (inputText.trim()) {
+      const memo: Memo = {
+        id: Date.now(),
+        text: inputText,
+        timestamp: new Date().toLocaleString("ja-JP"),
+      };
+      setMemos([memo, ...memos]);
+    }
+  };
+ 
+  return (
+    <Layout>
+      <div className="w-3/4 flex-col space-y-4">
+        <Editor
+          placeholder="新しいメモを入力.."
+          type="shadow"
+          onSubmit={addMemo}
+        />
+      </div>
+      {memos.map((memo) => (
+        <p key={memo.id}>{memo.text}</p>
+      ))}
+    </Layout>
+  );
 }
-
+ 
 export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,28 +4,30 @@ type LayoutProps = {
   
   const Layout = (props: LayoutProps) => {
     return (
-      <div className="px-24 py-12 w-full space-y-8 min-h-screen bg-gray-100">
-        <h1 className="text-5xl font-bold">My Simple Memo</h1>
-        <div className="grid grid-cols-3 gap-8 flex-1">
-          <main className="space-y-4 col-span-2">{props.children}</main>
-          <aside>{/* カスタムサイドバー */}</aside>
+        <div className="px-24 py-12 w-full space-y-8 min-h-screen bg-gray-100">
+          <h1 className="text-5xl font-bold">My Simple Memo</h1>
+          <div className="grid grid-cols-3 gap-8 flex-1">
+            <main className="space-y-4 col-span-2">{props.children}</main>
+     
+            <aside>{/* カスタムサイドバー */}</aside>
+          </div>
+     
+          <footer>
+            <p className="text-gray-700">
+              Created by{" "}
+              <a
+                className="text-emerald-600"
+                href="#"
+                target="_blank"
+                rel="noreferrer"
+              >
+                @your-handle
+              </a>{" "}
+              &copy; 2024
+            </p>
+          </footer>
         </div>
-        <footer>
-          <p className="text-gray-700">
-            Created by{" "}
-            <a
-              className="text-emerald-600"
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-            >
-              @your-handle
-            </a>{" "}
-            &copy; 2024
-          </p>
-        </footer>
-      </div>
-    );
+      );
   };
   
   export default Layout;
